9436e33b6ecedafaac2344a738191ea5d1db0c91
irvinzato <multizato@hotmail.com>
2022-06-14 23:24:13 -0500

    Creacion de primer ruta con respuesta

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const express = require('express');
 //Crear el servidor express
 const app = express();
 
+//Rutas
+app.get( '/', (req, res) => {
+    res.json({
+        ok: true,
+        msg: 'Hola desde el get /'
+    });
+});
+
 app.listen( 3000, () => {
     console.log("Servidor corriendo en puerto ", 3000);
 });
