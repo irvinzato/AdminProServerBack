34e82711169fde43e9afbad33bb5f1f7f7e06d32
irvinzato <multizato@hotmail.com>
2022-06-28 21:02:19 -0500

    Complete la subida y almacenamiento de archivos, utilice libreria uuid para evitar nombres iguales

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,4 +1,5 @@
 const { response } = require('express');
+const { v4: uuidv4 } = require('uuid'); //Para utilizarle instale "npm install uuid"
 
 //En mi carpeta "uploads" es donde colocare la imagen respectivamente
 //Utilice instalar en mi proyecto "npm i express-fileupload"
@@ -27,13 +28,42 @@ const subirArchivo = async (req, res = response) => {
     }
 
     //Procesar la imagen
+    const file = req.files.imagen;
 
-    res.json({
-        ok: true,
-        mmsg: 'Subida de archivo exitosa'
+    const nombreCortado = file.name.split('.');
+    const extensionArchivo = nombreCortado[ nombreCortado.length - 1 ];
+
+    //Validar extension
+    const extensionesValidas = [ 'png', 'jpg', 'jpeg', 'gif' ];
+    
+    if( !extensionesValidas.includes(extensionArchivo) ) {
+        return res.status(400).json({
+            ok: false,
+            msg: 'La extension no es permitida, debe ser png, jpg, jpeg o gif'
+        });
+    }
+
+    //Generar el nombre del archivo, de esta manera si los usuarios suben un archivo con el mismo nombre, yo le dare un identificador unico
+    const nombreArchivo = `${ uuidv4() }.${ extensionArchivo }`;
+
+    //Path donde guardare la imagen
+    const path = `./uploads/${ tipo }/${nombreArchivo}`;
+
+    //Mover la imagen
+    file.mv( path , (err) => {
+        if (err){
+            console.log("Error al mover imagen ", err);
+            return res.status(500).json({
+                ok: false,
+                msg: 'Error al guardar la imagen en su path'
+            });
+        }
+        res.json({
+            ok: true,
+            nombreArchivo,
+            msg: 'Subida de archivo exitosa'
+        });
     });
-
-
 }
 
 
